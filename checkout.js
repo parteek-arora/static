ec59c56a7e10be5cff9c6008eebad89544fa39a6
Parteek Arora <50576863+parteek-arora@users.noreply.github.com>
2020-12-21 19:39:37 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,1 +1,6 @@
-console.log("this is to test the static content")
+console.log("this is to test the static content");
+{{inject "checkoutId" checkout.id}};
+
+var jsContext = JSON.parse({{jsContext}});
+var checkoutId = jsContext.checkoutId;
+console.log(jsContext)
